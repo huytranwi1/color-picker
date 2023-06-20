0208ca143309941e4fa8856ec72a2ab16bf69434
huytranwi1 <huytranwii12@gmail.com>
2023-06-19 18:00:24 -0700

    finally got the function the work that make the navbar changed color picked

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 // Write your Color component here
-
-const Color = ({ color }) => {
-  return <div className={color}></div>;
-};
+import { useState } from "react";
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
       <div id="colors-list">
-        <Color color="blue" />
-        <Color color="violet" />
-        <Color color="black" />
+        <Color color="blue" setSelectedColor={setSelectedColor} />
+        <Color color="violet" setSelectedColor={setSelectedColor} />
+        <Color color="black" setSelectedColor={setSelectedColor} />
       </div>
     </div>
   );
+};
+
+const Color = ({ color, setSelectedColor }) => {
+  return <div className={color} onClick={() => setSelectedColor(color)}></div>;
 };
 
 export default App;
